e1c3d3a77f901000f25103203ed1a371eb0919c8
John Mc Murray <rftagvujea@www502.jnb1.host-h.net>
2017-10-23 12:27:49 +0200

    Added a sample Variables.inc.php file

---
+++ includes/Variables-Sample.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+// Database settings
+$DatabaseServerAddress = "localhost";
+$DatabaseName = "my-cool-db";
+$DatabaseUserName = "user";
+$DatabasePassword = "password123";
+$TablePrefix = "invoices_";
+?>
